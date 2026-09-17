87d5cc8821504774c052038e0036912bbbc6c0fe
jefferypauley <jeffery.pauley@atlasstudents.com>
2026-09-17 00:46:37 +0000

    test1

---
+++ 0-main.c
@@ -0,0 +1,12 @@
+#include "header.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])) {
+// print a welcome
+    printf("Welcome\n");
+
+    if (argc < 4) {
+        printf("HEY MAN, put 3 days of data\n");
+        return EXIT_FAILURE;
+    }
---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// Function for displaying step
+void print_all_steps(int steps);
+
+// Function for showing menu
+
+// Function for finding average
+
+#endif // 
